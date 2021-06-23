351889153b8b61b8fba4a019e861ccacb0d1cc59
luisEfe80 <luismendez80@hotmail.com>
2021-06-23 19:27:45 -0400

    Ajuste contenido t5

--- src/views/Tema5.vue
+++ src/views/Tema5.vue
@@ -30,76 +30,90 @@
       //- .color-primario, .color-secundario, .color-acento-contenido, .color-acento-botones
       //- estas clases tambien tienen un modificador --borde
       .tarjeta.color-acento-botones--borde.p-4(titulo="Paso 1: Guion")
-        h4 Paso 1: Guion
-        p This is a normal paragraph (<code>p</code> element). To add some length to it, let us mention that this page was primarily written for testing the effect of 
-          strong user style sheets
-          |. You can use it for various other purposes as well, like just checking how your browser displays various HTML elements by default. It can also be useful when testing conversions from HTML format to other formats, since some elements can go wrong then.
-        p This is another paragraph. I think it needs to be added that the set of elements tested is not exhaustive in any sense. I have selected those elements for which it can make sense to write user style sheet rules, in my opionion.
+        h4 Crear el guion
+        .row
+          .col-lg-2
+            figure.mb-4
+              img(src='@/assets/curso/t5-i2.svg', alt='Texto que describa la imagen')
+          .col-lg-10
+            p.mb-4  El guion es el punto de partida en la creación de un video, es donde se engancha al público y se expone lo que se logrará al finalizar de verlo. Se debe tener en cuenta:
+            ul.lista-ul
+              li 
+                i.fas.fa-angle-right
+                | Cautivar al público desde el inicio del video.
+              li 
+                i.fas.fa-angle-right
+                | Tener claro el tema que se desarrollará.
+              li 
+                i.fas.fa-angle-right
+                | Planear el contenido; es decir, cómo se va a entregar a los usuarios. Un ejemplo sería hacerlo paso a paso y dividir el tema en pequeños pasos que se entregarán en orden, para lograr una mayor efectividad al comunicar el mensaje.
+              li 
+                i.fas.fa-angle-right
+                | Saber cuáles imágenes, efectos, audios y demás recursos, apoyarán la producción del video.
+        
       
       //- .tarjeta debe ir acompañado de una de una de estas clases => 
       //- .color-primario, .color-secundario, .color-acento-contenido, .color-acento-botones
       //- estas clases tambien tienen un modificador --borde
       .tarjeta.color-acento-botones--borde.p-4(titulo="Paso 2: Escenario")
-        h4 Paso 2: Escenario
+        h4 Definir el escenario donde se realizará el video y las herramientas que se van a utilizar
 
         .row
-          .col-xl-6.mb-4
-            figure
-              img(src='@/assets/template/img-placeholder.svg', alt='Texto que describa la imagen')
-              figcaption Leyenda de la imagen
-          .col-xl-6
-            p This is a normal paragraph (<code>p</code> element). To add some length to it, let us mention that this page was primarily written for testing the effect of 
-              strong user style sheets
-              |. You can use it for various other purposes as well, like just checking how your browser displays various HTML elements by default. It can also be useful when testing conversions from HTML format to other formats, since some elements can go wrong then.
-            p This is another paragraph. I think it needs to be added that the set of elements tested is not exhaustive in any sense. I have selected those elements for which it can make sense to write user style sheet rules, in my opionion.
-      
-      //- .tarjeta debe ir acompañado de una de una de estas clases => 
-      //- .color-primario, .color-secundario, .color-acento-contenido, .color-acento-botones
-      //- estas clases tambien tienen un modificador --borde
+          .col-lg-2
+            figure.mb-4
+              img(src='@/assets/curso/t5-i3.svg', alt='Texto que describa la imagen')
+          .col-lg-10
+            p.mb-4  El escenario se puede definir de acuerdo con lo que se quiera proyectar al público objetivo. Existen varias opciones para tener como escenarios; sin embargo, se puede acondicionar una habitación, para que el entorno contenga los conceptos que se quieren transmitir. Lo importante es cuidar los detalles de la luz, el fondo del video y el audio. Con respecto al fondo, se puede hacer uso de cromas, una técnica visual que extrae el color de la imagen, y se reemplaza por otra.
+
       .tarjeta.color-acento-botones--borde.p-4(titulo="Paso 3: Grabación")
-        h4 Paso 3: Grabación
-        p This is a normal paragraph (<code>p</code> element). To add some length to it, let us mention that this page was primarily written for testing the effect of 
-          strong user style sheets
-          |. You can use it for various other purposes as well, like just checking how your browser displays various HTML elements by default. It can also be useful when testing conversions from HTML format to other formats, since some elements can go wrong then.
-        p This is another paragraph. I think it needs to be added that the set of elements tested is not exhaustive in any sense. I have selected those elements for which it can make sense to write user style sheet rules, in my opionion.
-      
-      //- .tarjeta debe ir acompañado de una de una de estas clases => 
-      //- .color-primario, .color-secundario, .color-acento-contenido, .color-acento-botones
-      //- estas clases tambien tienen un modificador --borde
-      .tarjeta.color-acento-botones--borde.p-4(titulo="Paso 4: Edición")
-        h4 Paso 4: Edición
-        p This is a normal paragraph (<code>p</code> element). To add some length to it, let us mention that this page was primarily written for testing the effect of 
-          strong user style sheets
-          |. You can use it for various other purposes as well, like just checking how your browser displays various HTML elements by default. It can also be useful when testing conversions from HTML format to other formats, since some elements can go wrong then.
-        p This is another paragraph. I think it needs to be added that the set of elements tested is not exhaustive in any sense. I have selected those elements for which it can make sense to write user style sheet rules, in my opionion.
-      
-      //- .tarjeta debe ir acompañado de una de una de estas clases => 
-      //- .color-primario, .color-secundario, .color-acento-contenido, .color-acento-botones
-      //- estas clases tambien tienen un modificador --borde
-      .tarjeta.color-acento-botones--borde.p-4(titulo="Paso 5: Exportar el video")
-        h4 Paso 5: Exportar el video
-        p This is a normal paragraph (<code>p</code> element). To add some length to it, let us mention that this page was primarily written for testing the effect of 
-          strong user style sheets
-          |. You can use it for various other purposes as well, like just checking how your browser displays various HTML elements by default. It can also be useful when testing conversions from HTML format to other formats, since some elements can go wrong then.
-        p This is another paragraph. I think it needs to be added that the set of elements tested is not exhaustive in any sense. I have selected those elements for which it can make sense to write user style sheet rules, in my opionion.
+        h4 Iniciar la grabación del video de acuerdo con el guion creado
+        .row
+          .col-lg-2
+            figure.mb-4
+              img(src='@/assets/curso/t5-i4.svg', alt='Texto que describa la imagen')
+          .col-lg-10
+            p.mb-4  Iniciar la grabación haciendo uso de la cámara que se ha seleccionado. Puede ser un celular o una cámara profesional. Lo importante en este paso, es tener en cuenta el guion que se creó, de manera que todos los movimientos y las palabras usadas tengan un sentido.
       
 
-    
-    //- El componente "Muestras" tiene las aplicaciones de todos los componentes de la base
-    //- se encuentra en src/components/Muestras.vue
-    //- Una vez el componente "Muestras" no se necesite 
-    //- Se debe borrar el "import Muestras from '../components/Muestras'" y en "components" en "<script"
-    //- esto evitará que se compile en la carpeta final de distribución
-    Muestras
+      .tarjeta.color-acento-botones--borde.p-4(titulo="Paso 4: Edición")
+        h4 Realizar la edición del video en la herramienta seleccionada
+        .row
+          .col-lg-2
+            figure.mb-4
+              img(src='@/assets/curso/t5-i5.svg', alt='Texto que describa la imagen')
+          .col-lg-10
+            p.mb-4  Se ha llegado a un punto importante dentro del proceso de creación: “la edición”. Aquí se unen muchos recursos diferentes a los captados en la grabación del video, como por ejemplo: imágenes, audios, animaciones, títulos, textos y más.
+      
+      .tarjeta.color-acento-botones--borde.p-4(titulo="Paso 5: Exportar el video")
+        h4 Exportar el video al formato que se requiera
+        .row
+          .col-lg-2
+            figure.mb-4
+              img(src='@/assets/curso/t5-i6.svg', alt='Texto que describa la imagen')
+          .col-lg-10
+            p.mb-4  En este paso se define en qué dispositivo se va a reproducir el video, qué formato necesita y en qué plataforma se va a difundir. Con respecto a los formatos, se encuentran los siguientes: AVI, WMV, MOV, FLV, RM, RMVB, MP4, MKV, 3GP.
+    .row.mb-5
+      .col-lg-10
+        p.mb-3  Una de las plataformas que se ha convertido en la herramienta preferida por los creadores de contenido es CapCut - Video Editor que es una aplicación de edición de video, todo en uno, que ayuda a crear videos increíbles. Corte, retroceder y cambiar la velocidad y mucho más, disponible para dispositivos móviles con sistema operativo iOS y Android.
+        p.mb-5  Invitamos a ver el siguiente video, donde se presenta una introducción al manejo de la herramienta Capcut.
+      .col-lg-2  
+        figure.mb-4
+          img(src='@/assets/curso/t5-i7.svg', alt='Texto que describa la imagen')
+    figure.mb-5
+      .video
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+    .row.justify-content-center.mb-5
+      .col-lg-10
+        .cajon.color-secundario.p-4 
+          p Hemos finalizado el estudio del diseño, planeación y utilización de herramientas para la creación de contenido digital. <b> ¡Anímate a crear tus propias piezas, tu creatividad es infinita! </b>
+
+
+
 </template>
 
 <script>
-import Muestras from '../components/Muestras' // borrar una vez el componente "Muestras" no se necesite
 export default {
   name: 'Tema5',
-  components: {
-    Muestras, // borrar una vez el componente "Muestras" no se necesite
-  },
   data: () => ({
     // variables de vue
   }),
