ce0d9bcd5494ab3804ceffbd50e857b0a5d24001
luisEfe80 <luismendez80@hotmail.com>
2021-07-06 02:40:43 -0400

    Ajuste de tabs

--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -8,32 +8,44 @@
         span  2
       h1 Herramientas para la construcción de una imagen
     p.mb-5  Las herramientas para la creación de imágenes, brindan una gama alta de posibilidades de edición en cuanto a filtros, fuentes de texto, formas, entre otras. Existen múltiples herramientas en el mercado, algunas gratuitas y otras pagas y, con respecto al sistema operativo, se pueden encontrar online algunas como por ejemplo:
-    .row.justify-content-center.mb-4
-      .col-lg-4
-        figure.mb-4
-          img.h100(src='@/assets/curso/t2-i1.svg', alt='Texto que describa la imagen')
-        .h4 Figma
-        p.mb-3
-        | Es un editor de gráficos, vectorial, y una herramienta de generación de prototipos, basada principalmente en la web, con características off-line adicionales, habilitadas por aplicaciones de escritorio en macOS y Windows.
-
-      .col-lg-4
-        figure.mb-4
-          img.h100(src='@/assets/curso/t2-i2.svg', alt='Texto que describa la imagen')
-        .h4 Canva
-        p.mb-3 Fundado en 2012, es un software y un sitio web de herramientas de diseño gráfico simplificado. Utiliza un formato de arrastrar y soltar, y proporciona acceso a más de 60 millones de fotografías y 5 millones de vectores, gráficos y fuentes.
-    .row.justify-content-center.mb-5
-      .col-lg-4
-        figure.mb-4
-          img.h100(src='@/assets/curso/t2-i3.svg', alt='Texto que describa la imagen')
-        .h4 Gimp
-        p.mb-3  Es un programa de edición de imágenes digitales, en forma de mapa de bits, tanto de dibujos como de fotografías. Es un programa libre y gratuito.
-
-      .col-lg-4
-        figure.mb-4
-          img.h100(src='@/assets/curso/t2-i4.svg', alt='Texto que describa la imagen')
-        .h4 Pixlr
-        p.mb-3  Es un conjunto de herramientas y utilidades de imágenes, basado en la nube, que incluye varios editores de fotografías y un servicio para compartirlas.
     
+    .row.justify-content-center.mb-4
+      .col-lg-8
+        TabsB.color-acento-contenido.mb-5
+          .py-4.py-md-5(titulo="Figma" :icono="require('@/assets/curso/t2-ic1.svg')")
+            .row
+              .col-md-6.mb-4.mb-md-0
+                p Es un editor de gráficos, vectorial, y una herramienta de generación de prototipos, basada principalmente en la web, con características off-line adicionales, habilitadas por aplicaciones de escritorio en macOS y Windows.
+              .col-md-6
+                figure
+                  img(src='@/assets/curso/t2-i1.svg', alt='Texto que describa la imagen')
+
+          .py-4.py-md-5(titulo="Canva" :icono="require('@/assets/curso/t2-ic2.svg')")
+            .row
+              .col-md-6.mb-4.mb-md-0
+                p Fundado en 2012, es un software y un sitio web de herramientas de diseño gráfico simplificado. Utiliza un formato de arrastrar y soltar, y proporciona acceso a más de 60 millones de fotografías y 5 millones de vectores, gráficos y fuentes.
+              .col-md-6
+                figure
+                  img(src='@/assets/curso/t2-i2.svg', alt='Texto que describa la imagen')
+            
+          .py-4.py-md-5(titulo="Gimp" :icono="require('@/assets/curso/t2-ic3.svg')")
+            .row
+              .col-md-6.mb-4.mb-md-0
+                p Es un programa de edición de imágenes digitales, en forma de mapa de bits, tanto de dibujos como de fotografías. Es un programa libre y gratuito.
+              .col-md-6
+                figure
+                  img(src='@/assets/curso/t2-i3.svg', alt='Texto que describa la imagen')
+
+
+          .py-4.py-md-5(titulo="Pixlr" :icono="require('@/assets/curso/t2-ic4.svg')")
+            .row
+              .col-md-6.mb-4.mb-md-0
+                p Es un conjunto de herramientas y utilidades de imágenes, basado en la nube, que incluye varios editores de fotografías y un servicio para compartirlas.
+              .col-md-6
+                figure
+                  img(src='@/assets/curso/t2-i4.svg', alt='Texto que describa la imagen')
+
+  
 
     .neg-margin.mb-5.py-5.tarjeta--gris
       .row.justify-content-center.mb-5
